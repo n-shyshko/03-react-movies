d152a7c873521fb34da9f3ff8f6e7a5dd9c177a7
nik <nshishko@gmail.com>
2025-09-17 20:44:13 +0300

    fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+// конфіг для Vite, щоб підтягувати змінні з .env
+export default defineConfig(({ mode }) => {
+  // loadEnv завантажує .env для поточного mode
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    define: {
+      // робимо змінні доступними як process.env.*, якщо потрібно
+      "process.env": env,
+    },
+  };
+});
