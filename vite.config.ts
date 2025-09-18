dcea956c4e477be9e015be664272d7a5266fd6bf
nik <nshishko@gmail.com>
2025-09-18 08:42:02 +0300

    fix final

--- vite.config.ts
+++ vite.config.ts
@@ -8,20 +8,3 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
 });
-
-// import { defineConfig, loadEnv } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // конфіг для Vite, щоб підтягувати змінні з .env
-// export default defineConfig(({ mode }) => {
-//   // loadEnv завантажує .env для поточного mode
-//   const env = loadEnv(mode, process.cwd(), "");
-
-//   return {
-//     plugins: [react()],
-//     define: {
-//       // робимо змінні доступними як process.env.*, якщо потрібно
-//       "process.env": env,
-//     },
-//   };
-// });
